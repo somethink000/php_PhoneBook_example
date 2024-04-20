814616988502c27ae819b9606cea0cc68a0f3918
some <zubkoffalex4nder@yandex.ru>
2024-04-20 12:28:49 +0300

    prepair

--- src/index.php
+++ src/index.php
@@ -8,7 +8,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>About</title>
+        <title>Телефонная книга</title>
         <link rel="stylesheet" href="/style/style.css">
     </head>
    
