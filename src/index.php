e56d90f75ac55a50fdb83e22d604915718d57622
some <zubkoffalex4nder@yandex.ru>
2024-04-20 14:23:22 +0300

    styles

--- src/index.php
+++ src/index.php
@@ -11,38 +11,20 @@
         <title>Телефонная книга</title>
         <link rel="stylesheet" href="/style/style.css">
     </head>
-   
     
 
-    <body class="body blur">
-        
-        <div class="particles_container">
-            <?php for ($i = 1; $i <= 100; $i++) { ?>   
-                <div class="circle-container">
-                    <div class="circle"></div>
-                </div>
-            <?php } ?>
-        </div>
+    <body class="blur">
+        <div class="main_container">
 
-
-        <div class="container">
-
-            <div class="about_box"> 
-
-                <img class="circle-image" src="/assets/myface.jpg"/>
-                <h1 class="title">Some</h1>
-                <h1 class="txt">Just another creator who love make things</h1>
-                
-                <div class="circle_links">
-                    <a href="https://sketchfab.com/susanin.krumbelvelikolep"><img class="link-image" src="/assets/sketchfab.svg"></a>
-                    <a href="https://steamcommunity.com/profiles/76561198412172044/"><img class="link-image" src="/assets/steam.svg"></a>
-                    <a href="https://www.youtube.com/channel/UCY2EUY4h6knWxZG1n8UEjlQ"><img class="link-image" src="/assets/youtube.svg"></a>
-                    <a href="https://github.com/somethink000"><img class="link-image" src="/assets/github.svg"></a>
-                </div>
-                
+            <div class="phones_box">
+                 
             </div>
+
+            <div class="add_form_box">
+                 
+            </div>
+
         </div>
-       
     </body> 
 
 </html>
