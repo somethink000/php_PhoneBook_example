804ac1635ab9a196b962f9ed6a885975483dea4b
some <zubkoffalex4nder@yandex.ru>
2024-04-20 11:54:01 +0300

    init

---
+++ src/index.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>About</title>
+        <link rel="stylesheet" href="/style/style.css">
+    </head>
+   
+    
+
+    <body class="body blur">
+        
+        <div class="particles_container">
+            <?php for ($i = 1; $i <= 100; $i++) { ?>   
+                <div class="circle-container">
+                    <div class="circle"></div>
+                </div>
+            <?php } ?>
+        </div>
+
+
+        <div class="container">
+
+            <div class="about_box"> 
+
+                <img class="circle-image" src="/assets/myface.jpg"/>
+                <h1 class="title">Some</h1>
+                <h1 class="txt">Just another creator who love make things</h1>
+                
+                <div class="circle_links">
+                    <a href="https://sketchfab.com/susanin.krumbelvelikolep"><img class="link-image" src="/assets/sketchfab.svg"></a>
+                    <a href="https://steamcommunity.com/profiles/76561198412172044/"><img class="link-image" src="/assets/steam.svg"></a>
+                    <a href="https://www.youtube.com/channel/UCY2EUY4h6knWxZG1n8UEjlQ"><img class="link-image" src="/assets/youtube.svg"></a>
+                    <a href="https://github.com/somethink000"><img class="link-image" src="/assets/github.svg"></a>
+                </div>
+                
+            </div>
+        </div>
+       
+    </body> 
+
+</html>
